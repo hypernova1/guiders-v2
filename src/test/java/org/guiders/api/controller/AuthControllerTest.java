5c5b49ae0003f452a54173c8a0bf101ebdaa3d9d
hypernova1 <chtlstjd01@gmail.com>
2020-04-24 20:45:53 +0900

    yaml 분리

--- src/test/java/org/guiders/api/controller/AuthControllerTest.java
+++ src/test/java/org/guiders/api/controller/AuthControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 class AuthControllerTest {
 
     @Autowired
